a9f5aa6b2c493c193d4d6bfd7d96f28fc51932fc
2u3j6 <1398433233@qq.com>
2025-07-25 14:58:28 +0800

    fix 优化

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,10 +1,8 @@
 import Form from '@/app/ui/invoices/edit-form'
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs'
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data'
-interface Props {
-  params: { id: string }
-}
-export default async function Page({ params }: Props) {
+
+export default async function Page({ params }: any) {
   const id = params.id
 
   const [invoice, customers] = await Promise.all([fetchInvoiceById(id), fetchCustomers()])
